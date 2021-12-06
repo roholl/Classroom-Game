79f7d0c3d053e3b6b8b3f757a622d1a78c23e23d
Shykoo-rethgaul <shykoo.rethgaul@gmail.com>
2021-12-06 10:30:39 -0600

    added player character
    -movement added to player
    -set up tile mapping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    float playerSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetPlayerInput();
     }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    public void GetPlayerInput()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    public void MovePlayer()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + playerSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + playerSpeed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
 }
